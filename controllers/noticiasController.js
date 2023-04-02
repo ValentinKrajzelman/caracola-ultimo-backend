0abd669e32acc797756b9e0a479d40dfd381d9c0
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2023-04-02 11:06:19 -0300

    cosas

--- controllers/noticiasController.js
+++ controllers/noticiasController.js
@@ -1,14 +1,115 @@
-
 import express from 'express';
+import mongoose from 'mongoose';
 
-import { getTalleres, createTalleres, getTalleres, updateTalleres, deleteTalleres } from '../controllers/talleresController.js';
+import EventoModelo from '../models/evento.js';
 
 const router = express.Router();
 
-router.get('/', getTalleres);
-router.post('/', createTalleres);
-router.get('/:id', getTalleres);
-router.patch('/:id', updateTalleres);
-router.delete('/:id', deleteTalleres);
+export const getEventos = async (req,res) => {
+    try {
+        const eventos = await EventoModelo.find();
+        res.status(200).json(eventos);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+export const createEventos = async (req,res) => {
+    try {
+        const eventos = await EventoModelo.find();
+        res.status(200).json(eventos);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+export const getOneEventos = async (req,res) => {
+    try {
+        const eventos = await EventoModelo.find();
+        res.status(200).json(eventos);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+export const updateEventos = async (req,res) => {
+    try {
+        const eventos = await EventoModelo.find();
+        res.status(200).json(eventos);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+export const deleteEventos = async (req,res) => {
+    try {
+        const eventos = await EventoModelo.find();
+        res.status(200).json(eventos);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+
+
+
+
+
+
+export const getPosts = async (req, res) => { 
+    try {
+        const postMessages = await PostMessage.find();
+                
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+
+    const { title, message, selectedFile, creator, tags } = req.body;
+
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully." });
+}
 
 export default router;
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+import * as dotenv from 'dotenv' 
+dotenv.config()
+
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postRoutes from './routes/posts.js';
+import {v2} from 'cloudinary';
+
+import postRoutes from './routes/eventosRoutes.js';
+// import postRoutes from './routes/noticiasRoutes.js';
+// import postRoutes from './routes/talleresRoutes.js';
 
 const app = express();
+
+v2.config({
+  cloud_name: process.env.CLOUND_NAME,
+  api_key:process.env.API_KEY,
+  api_secret:process.env.API_SECRET,
+  secure: true,
+})
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -13,11 +27,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://valentinkrajzelman:x6MYYndOxAtpJEwC@dbmemorias.acfpnm5.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://valentinkrajzelman:x6MYYndOxAtpJEwC@dbmemorias.acfpnm5.mongodb.net/?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
